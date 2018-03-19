63206d87555d09d1749ac7947964106cb2b5cf6b
“YOUR <shnnagai@gmail.com>
2018-03-19 00:25:42 -0400

    update chi-square

--- Exam1-extra-credit/chi-square.py
+++ Exam1-extra-credit/chi-square.py
@@ -50,12 +50,69 @@ class chi_square:
         w1Nw2 = self.w1_And_Not_w2(word1, text)
         Nw1w2 = self.not_w1_And_w2(word2, text)
         Nw1Nw2 = self.not_w1_and_not_w2(w1w2, text)
+        xsquare = self.xSquare(w1w2, w1Nw2, Nw1w2, Nw1Nw2)
         print("C(w1) :          # ", w1)
         print("C(w2) :          # ", w2)
         print("C(w1w2):         # ", w1w2)
         print("C(w1 && !w2):    # ", w1Nw2)
         print("C(!w1 && w2):    # ", Nw1w2)
         print("C(!w1 && !w2):   # ", Nw1Nw2)
+        print("\n\n'0.05% Baseline: 3.841'\nX^2# ", xsquare)
+
+    def xSquare(self, w1w2, w1Nw2, Nw1w2, Nw1Nw2):
+        if w1w2 == 0:
+            w1w2 = 1
+        # percentage
+        w2_percent = (w1w2 + Nw1w2)
+        Nw2_percent = (w1Nw2 + Nw1Nw2)
+        total = w2_percent + Nw2_percent
+        w2_percent = w2_percent / total
+        Nw2_percent = Nw2_percent / total
+        # total w1
+        w1_total = w1w2 + w1Nw2
+        Nw1_total = Nw1w2 + Nw1Nw2
+        # expected value
+        # w1w2_expected = w1_total * w2_percent
+        # w1Nw2_expected = w1_total * Nw2_percent
+        # Nw1w2_expected = Nw1_total * w2_percent
+        # Nw1Nw2_expected = Nw1_total * Nw2_percent
+        print("C(w1w2):         # ", w1w2)
+        print("C(w1 && !w2):    # ", w1Nw2)
+        print("C(!w1 && w2):    # ", Nw1w2)
+        print("C(!w1 && !w2):   # ", Nw1Nw2)
+        print("w2_percent :     # ", w2_percent)
+        print("Nw2_percent :    # ", Nw2_percent)
+        print("w1_total :       # ", w1_total)
+        print("Nw1_total :       # ", Nw1_total)
+
+        sum = 0
+        i = 0
+        observedValue = [
+            w1w2,
+            w1Nw2,
+            Nw1w2,
+            Nw1Nw2
+        ]
+        expectedValues = [
+            w1_total * w2_percent,
+            w1_total * Nw2_percent,
+            Nw1_total * w2_percent,
+            Nw1_total * Nw2_percent
+        ]
+
+        for i in range(4):
+
+            numerator1 = (observedValue[i] - expectedValues[i])
+            numerator2 = math.pow(numerator1, 2)  # x^2
+            x = numerator2/observedValue[i]
+
+            print(observedValue[i], ' - ', expectedValues[i])
+            print(' X^2 ', math.pow(numerator1, 2))
+            print('sum', numerator2/observedValue[i])
+            print('\n\n')
+            sum = sum + x
+
+        return sum
 
     def singleWord(self, word, text):
         text = ' '.join(text)
@@ -87,9 +144,11 @@ class chi_square:
         return len(totalBigrams) - w1w2
 
 
+from nltk.corpus import brown
+brown = brown.words()
 c = chi_square()
 c.getChiSquare("new", "companies", brown)
 
 
-t = Tvalue()
-t.generateTvalue("have", "had", "to", brown)
+# t = Tvalue()
+# t.generateTvalue("have", "had", "to", brown)
