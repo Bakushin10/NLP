f1410826eee095ba4efe0c53554175bb2aba5ebe
“YOUR <shnnagai@gmail.com>
2018-04-06 16:38:27 -0400

    feature implemented

--- ec2/ec2.py
+++ ec2/ec2.py
@@ -7,54 +7,80 @@ import nltk
 class EC2:
     def loadFile(self):
         text = ""
-        document = []
+        humanWords = []
+        robotsWords = []
         for eachFile in glob.glob("Desktop/UF/Spring-2018/NLP/github-repo/NLP/ec2/Loebner-logs-2003/ALICE/*.log"):
             with codecs.open(eachFile, "r", encoding='utf-8', errors='ignore') as file:
                     print("opening...", eachFile)
-                    text = file.read()
-                    PROGRAM_list = self.trimPROGRAM(text)
-                    JUDGE_list = self.trimJUDGE(text)
-                    print("PROGRAM_list : ", len(PROGRAM_list))
-                    print("JUDGE_list : ", len(JUDGE_list))
-
-                    # build the document
-                    for i in range(len(JUDGE_list)):
-                        index = {
-                             PROGRAM_list[i],
-                             JUDGE_list[i]
-                            }
-                        document.append(index)
-                    print(len(document))
-
-        features = self.build_feature(document)
-
+                    text = file.readlines()
+                    PROGRAM_list = self.trimPROGRAM(text, humanWords, robotsWords)
+                    # print(humanWords)
+                    # print("______________________________\n\n\n")
+                    # print(robotsWords)
+        humanWords = self.joinList(humanWords)
+        robotsWords = self.joinList(robotsWords)
+        features = humanWords + robotsWords
+        print(features)
+        print(len(features))
+        # self.build_feature(humanWords)
     # I need to create the feature based on the document
     # document contains the list of conversation
     # print(document)
     def build_feature(self, document):
-        feature_set = [(p, j) for p, j in document]  # p = program, j = judge
-        return feature_set
+        features = {}
+        for list in document:
+            features = [(word, entity) for word, entity in list]
+        print(features)
+        # feature_set = [(p, j) for p, j in document]  # p = program, j = j
+        # return feature_set
 
-    def trimPROGRAM(self, text):
-        findProgram = re.compile('PROGRAM: .*\r')
-        trimmed = findProgram.findall(text)
-        newTrimmed = []
-        for word in trimmed:
-            sent = word.replace("PROGRAM: ", "")  # removing 'PROGRAM :'
-            sent = sent.replace("\r", "")         # removing '\r'
-            newTrimmed.append(sent)
-        return newTrimmed  # return the list of program
+    def trimPROGRAM(self, text, humanWords, robotsWords):
+        findHuman = re.compile('CONFEDERATE')
+        findHuman2 = re.compile('JUDGE')
+        extractText = re.compile('[A-Z]*: .*\r')
+        for line in text:
+            fh = findHuman.findall(line)
+            fh2 = findHuman2.findall(line)
 
-    def trimJUDGE(self, text):
-        findProgram = re.compile('JUDGE: .*\r')
-        trimmed = findProgram.findall(text)
-        newTrimmed = []
-        for word in trimmed:
-            sent = word.replace("JUDGE: ", "")
-            sent = sent.replace("\r", "")
-            newTrimmed.append(sent)
-        return newTrimmed  # return the list of program
+            if fh == [] and fh2 == []:
+                message = extractText.findall(line)
+                onlyText = self.removeCapitalSequence(message)
+                newList = self.label_word(onlyText, "ROBOTS")
+                robotsWords.append(newList)
+                # print("ROBOTS:", onlyText)
+            else:
+                message = extractText.findall(line)
+                onlyText = self.removeCapitalSequence(message)
+                newList = self.label_word(onlyText, "HUMAN")
+                humanWords.append(newList)
+                # print("HUMAN:", onlyText)
 
+    def label_word(self, text, entity):
+        text = nltk.word_tokenize(text)
+        labeled_word = [(word, entity) for word in text]
+        return labeled_word
+
+    def joinList(self, lists):
+        mergeList = []
+        for list in lists:
+            mergeList = mergeList + list
+        return mergeList
+
+    def removeCapitalSequence(self, message):
+        removed = False
+        removeWord = ""
+        message = ''.join(message)
+        message = message.replace('\r', "")
+        trimmedMessage = ""
+        for c in message:
+            if c == ':' and removed is False:
+                removeWord = removeWord + c
+                trimmedMessage = message.replace(removeWord, "")
+                removed = True
+            elif c.isupper() and removed is False:
+                removeWord = removeWord + c
+        return trimmedMessage
+        
 
 ec = EC2()
 ec.loadFile()
